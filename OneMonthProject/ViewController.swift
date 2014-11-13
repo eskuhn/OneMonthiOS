05855a49eae84b1293805d21acb61ddc18e0b6a7
eskuhn <eskuhn@gmail.com>
2014-11-13 14:32:37 -0800

    Initial Commit
    First commit — empty application

---
+++ OneMonthProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OneMonthProject
+//
+//  Created by Eric S Kuhn on 11/13/14.
+//  Copyright (c) 2014 Eric S Kuhn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
