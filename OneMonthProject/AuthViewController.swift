598e6236ac0658a932ccc745362ccf706f7ed7bb
eskuhn <eskuhn@gmail.com>
2014-12-07 09:48:18 -0800

    Follow Self
    User with follow him/herself at successful signup

--- OneMonthProject/AuthViewController.swift
+++ OneMonthProject/AuthViewController.swift
@@ -137,6 +137,20 @@ class AuthViewController: UIViewController, UITextFieldDelegate {
             
             if error == nil
             {
+                // New user follows him/herself
+                self.follow(user, completionHandler: {
+                    (error) -> () in
+                    
+                    if error == nil
+                    {
+                        var tabBarController = TabBarController()
+                        self.navigationController?.pushViewController(tabBarController, animated: true)
+                    }
+                    else
+                    {
+                        println("unable for user to follow themself")
+                    }
+                })
                 var tabBarController = TabBarController()
                 self.navigationController?.pushViewController(tabBarController, animated: true)
             }
@@ -147,6 +161,16 @@ class AuthViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
+    func follow(user: PFUser!, completionHandler:(error: NSError?) -> ())
+    {
+        var relation = PFUser.currentUser().relationForKey("following")
+        relation.addObject(user)
+        PFUser.currentUser().saveInBackgroundWithBlock({
+            (success, error) -> Void in
+            
+            completionHandler(error: error)
+        })
+    }
 }
     
     
